1eeba8cf2a73c98b128e1d675786be418d9825b0
Marina Batina <mbatina.oat@gmail.com>
2023-09-11 10:29:19 +0300

    Introduce My Profile page [wip]

--- app/personal-page/my-profile/page.jsx
+++ app/personal-page/my-profile/page.jsx
@@ -1,10 +1,34 @@
+"use client";
 
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 const MyProfile =()=>{
+
+    const {data: session} = useSession();
+
     return (
-        <>
-        My profile
-        </>
+        <div className="flex justify-items-start align-center gap-20 ml-40 mt-20">
+            <diV>
+                   <Image src= {session?.user.image ? session?.user.image: "/assets/images/user-image.png"} 
+                   width={110} height={55}  loading="eager" className='rounded-full' alt='profile'/>
+                   <h3>Change Photo</h3>
+            </diV>
+            <div className='flex flex-col gap-4'>
+                <div>
+                     My email:<span>{session?.user.email} </span>
+                </div>
+                <div>
+                     My source Language:
+                </div>
+                 <div>
+                    My Target Language:
+                </div>
+                <div>
+                    Delete Profile
+                </div>      
+            </div>
+        </div>
     )
 }
 
