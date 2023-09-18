f50315ffeb0451560117deba317ca0660a3685b6
Marina Batina <mbatina.oat@gmail.com>
2023-09-18 17:41:17 +0300

    Work with Responsive CSS[wip]

--- components/MyFooter.jsx
+++ components/MyFooter.jsx
@@ -6,7 +6,7 @@ const MyFooter = ()=>{
     return(
         <div className="main-footer lg:text-sm bg-slate-400 flex   bg-opacity-70 sm:justify-between items-center md:text-xs ">
             <div className=" flex gap-4 text-gray-700/75">
-                <p className="copyright-footer ml-8 sm:ml-2">Copyright © {year} Marina Batina. All rights reserved &nbsp; &nbsp; | &nbsp;</p>
+                <p className="copyright-footer ml-8 sm:ml-2">Copyright © {year} Marina Batina.<span className="rights-footer">All rights reserved &nbsp; &nbsp; | &nbsp;</span> </p>
                 <p className="v-footer sm:hidden">Lingua Cat v.0.1.1 &nbsp;&nbsp;|</p>
                 <p className="flaticon-footer md:hidden lg:block sm:hidden">Icons from 
                     <Link href="https://www.flaticon.com/"className="hover:text-emerald-700 text-emerald-900"> &nbsp;Flaticon &nbsp; </Link>
